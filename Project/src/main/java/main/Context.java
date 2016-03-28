931d239945260858e059e888c2e8b10fd405235f
Ксения Сначева <Snach@MacBook-Air-Ksenia.local>
2016-03-28 13:50:05 +0300

    fix Context v.2

--- Project/src/main/java/main/Context.java
+++ Project/src/main/java/main/Context.java
@@ -4,6 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import java.util.HashMap;
 import java.util.Map;
+import java.lang.IllegalStateException;
 
 /**
  * Created by Snach on 26.03.16.
@@ -11,24 +12,30 @@ import java.util.Map;
 public class Context {
     @NotNull
     private final Map<Class, Object> contextMap = new HashMap<>();
+    @SuppressWarnings("ConstantNamingConvention")
+    private static final Logger logger = new Logger(Context.class);
 
     public void put(@NotNull Class clazz, @NotNull Object object){
-        if (contextMap.containsKey(clazz))
-            System.out.append("В Context уже есть класс " + clazz.getName() );
-        else
+        try {
+            contextMap.containsKey(clazz);
             contextMap.put(clazz, object);
+        }
+        catch (IllegalStateException e) {
+            logger.log("В Context уже есть класс " + clazz.getName() );
+        }
     }
 
     @Nullable
     public <T> T get(@NotNull Class<T> clazz){
 
-        try {
+        //noinspection unchecked
+        if (contextMap.get(clazz) == null) {
+            logger.log("В Context нет класса " + clazz.getName());
+            return null;
+        }
+        else {
             //noinspection unchecked
             return (T) contextMap.get(clazz);
-        }
-        catch (NullPointerException e) {
-            System.out.append("В Context нет класса " + clazz.getName());
-            return null;
         }
     }
 }
